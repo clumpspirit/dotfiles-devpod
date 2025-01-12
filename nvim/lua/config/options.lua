f2d65d0953544239aae10c6221166f547ca90263
clumpspirit <edindzilic@gmail.com>
2025-01-11 19:35:27 -0700

    feat: add nvim and lazygit

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,14 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+local opt = vim.opt
+
+-- try this: vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
+opt.ignorecase = true
+
+-- scrolling
+opt.number = false
+opt.relativenumber = false
+opt.scrolloff = 8
---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+  -- add gruvbox
+  { "wittyjudge/gruvbox-material.nvim" },
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox-material",
+    },
+  }
+}
